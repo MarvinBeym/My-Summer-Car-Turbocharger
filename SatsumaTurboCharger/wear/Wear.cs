25495fb41305c5b73886a521b3882b87ad0f9db1
Marvin Beym <Marvin.Beym@gmx.de>
2023-08-05 17:02:07 +0200

    Temporary comment out wear classes until reimplemented

--- SatsumaTurboCharger/wear/Wear.cs
+++ SatsumaTurboCharger/wear/Wear.cs
@@ -9,14 +9,16 @@ using Random = System.Random;
 
 namespace SatsumaTurboCharger.wear
 {
-    public class WearMultiplicatorManipulator
-    {
-        internal float[] manipulatorValues;
-        public WearMultiplicatorManipulator(float[] manipulatorValues)
-        {
-            this.manipulatorValues = manipulatorValues;
-        }
-    }
+	/*
+	 * ToDo: Requires reimplementation
+	public class WearMultiplicatorManipulator
+	{
+		internal float[] manipulatorValues;
+		public WearMultiplicatorManipulator(float[] manipulatorValues)
+		{
+			this.manipulatorValues = manipulatorValues;
+		}
+	}
 
 	public class Wear
 	{
@@ -26,25 +28,25 @@ namespace SatsumaTurboCharger.wear
 		public WearLogic activeLogic;
 		public WearLogic rigidLogic;
 
-        public List<WearCondition> wearConditions;
-        public float wear = 100;
-        public float wearReductionMultiplier;
-        public float wearReductionIntervall;
-        public int randomFallOff = -1;
+		public List<WearCondition> wearConditions;
+		public float wear = 100;
+		public float wearReductionMultiplier;
+		public float wearReductionIntervall;
+		public int randomFallOff = -1;
 
-        public float timer = 0;
+		public float timer = 0;
 
-        internal float[] multiplicatorManipulator;
+		internal float[] multiplicatorManipulator;
 
-        //Shop
-        private ShopItem modsShop;
-        private ProductDetails repairProduct;
-        public bool repairPurchaseMade = false;
+		//Shop
+		private ShopItem modsShop;
+		private ProductDetails repairProduct;
+		public bool repairPurchaseMade = false;
 
-        public override string ToString()
-        {
-            return wear.ToString("000.00000");
-        }
+		public override string ToString()
+		{
+			return wear.ToString("000.00000");
+		}
 
 		public Wear(SatsumaTurboCharger mod, string id, Part part, List<WearCondition> wearConditions, float wearReductionMultiplier, float wearReductionIntervall, Dictionary<string, float> wearSave, float productPrice, string productImage, int randomFallOff = -1, float[] multiplicatorManipulator = null)
 		{
@@ -56,15 +58,15 @@ namespace SatsumaTurboCharger.wear
 			this.part = part;
 			this.multiplicatorManipulator = multiplicatorManipulator;
 
-            try
-            {
-                this.wear = wearSave[id];
-            }
-            catch
-            {
-                this.wear = 100;
-            }
-            this.randomFallOff = randomFallOff;
+			try
+			{
+				this.wear = wearSave[id];
+			}
+			catch
+			{
+				this.wear = 100;
+			}
+			this.randomFallOff = randomFallOff;
 
 
 			activeLogic = part.AddWhenUninstalledBehaviour<WearLogic>();
@@ -86,21 +88,21 @@ namespace SatsumaTurboCharger.wear
 
 			modsShop = Cache.Find("Shop for mods").GetComponent<ShopItem>();
 
-            repairProduct = new ProductDetails
-            {
-                productName = String.Format("REPAIR {0}", part.activePart.name.Replace("(Clone)", "")),
-                multiplePurchases = false,
-                productCategory = "DonnerTech Racing",
-                productIcon = mod.assetsBundle.LoadAsset<Sprite>(productImage),
-                productPrice = 4000
-            };
-            modsShop.Add(mod, repairProduct, ShopType.Fleetari, RepairPurchase, null);
+			repairProduct = new ProductDetails
+			{
+				productName = String.Format("REPAIR {0}", part.activePart.name.Replace("(Clone)", "")),
+				multiplePurchases = false,
+				productCategory = "DonnerTech Racing",
+				productIcon = mod.assetsBundle.LoadAsset<Sprite>(productImage),
+				productPrice = 4000
+			};
+			modsShop.Add(mod, repairProduct, ShopType.Fleetari, RepairPurchase, null);
 
-        }
+		}
 
-        public void RepairPurchase(PurchaseInfo purchaseInfo)
-        {
-            repairPurchaseMade = true;
+		public void RepairPurchase(PurchaseInfo purchaseInfo)
+		{
+			repairPurchaseMade = true;
 
 			if (part.IsInstalled || !part.activePart.activeSelf || !Helper.CheckCloseToPosition(part.activePart.transform.position, ModsShop.FleetariSpawnLocation.desk, 0.8f))
 			{
@@ -109,94 +111,95 @@ namespace SatsumaTurboCharger.wear
 				return;
 			}
 
-            wear = 100;
-            //Color has to then also be reset
+			wear = 100;
+			//Color has to then also be reset
 
-            part.activePart.transform.position = FleetariSpawnLocation.desk;
-            part.activePart.SetActive(true);
-        }
+			part.activePart.transform.position = FleetariSpawnLocation.desk;
+			part.activePart.SetActive(true);
+		}
 
-        public void ResetModsShopRepairPurchase()
-        {
-            repairPurchaseMade = false;
-            List<ShopItems> shopItems = modsShop.fleetariShopItems;
-            foreach (ShopItems shopItem in shopItems)
-            {
-                if(shopItem.details.productName == repairProduct.productName)
-                {
-                    shopItem.purchashed = false;
-                }
-            }
-        }
+		public void ResetModsShopRepairPurchase()
+		{
+			repairPurchaseMade = false;
+			List<ShopItems> shopItems = modsShop.fleetariShopItems;
+			foreach (ShopItems shopItem in shopItems)
+			{
+				if (shopItem.details.productName == repairProduct.productName)
+				{
+					shopItem.purchashed = false;
+				}
+			}
+		}
 
-        public float CalculateWearResult(float valueToManipulate)
-        {
-            float manipulatedValue = valueToManipulate;
-            float manipulator = 1;
-            timer += Time.deltaTime;
-            for (int i = 0; i < wearConditions.Count; i++)
-            {
-                if (multiplicatorManipulator != null)
-                {
-                    manipulator = multiplicatorManipulator[i];
-                }
-                WearCondition wearCondition = wearConditions[i];
-                WearCondition.Check check = wearCondition.conditionCheck;
-                if (check == WearCondition.Check.Equal)
-                {
-                    if (wear == wearCondition.check) { manipulatedValue /= wearCondition.divider; break; }
-                }
-                else if (check == WearCondition.Check.MoreThan)
-                {
-                    if (wear >= wearCondition.check) { manipulatedValue /= wearCondition.divider; break; }
-                }
-                else if (check == WearCondition.Check.LessThan)
-                {
-                    if (wear <= wearCondition.check) { manipulatedValue /= wearCondition.divider; break; }
-                    HandleRandomFalloff();
-                }
-            }
+		public float CalculateWearResult(float valueToManipulate)
+		{
+			float manipulatedValue = valueToManipulate;
+			float manipulator = 1;
+			timer += Time.deltaTime;
+			for (int i = 0; i < wearConditions.Count; i++)
+			{
+				if (multiplicatorManipulator != null)
+				{
+					manipulator = multiplicatorManipulator[i];
+				}
+				WearCondition wearCondition = wearConditions[i];
+				WearCondition.Check check = wearCondition.conditionCheck;
+				if (check == WearCondition.Check.Equal)
+				{
+					if (wear == wearCondition.check) { manipulatedValue /= wearCondition.divider; break; }
+				}
+				else if (check == WearCondition.Check.MoreThan)
+				{
+					if (wear >= wearCondition.check) { manipulatedValue /= wearCondition.divider; break; }
+				}
+				else if (check == WearCondition.Check.LessThan)
+				{
+					if (wear <= wearCondition.check) { manipulatedValue /= wearCondition.divider; break; }
+					HandleRandomFalloff();
+				}
+			}
 
-            if (timer >= wearReductionIntervall)
-            {
-                timer = 0;
-                wear -= valueToManipulate * wearReductionMultiplier;
-            }
+			if (timer >= wearReductionIntervall)
+			{
+				timer = 0;
+				wear -= valueToManipulate * wearReductionMultiplier;
+			}
 
-            return manipulatedValue;
-        }
+			return manipulatedValue;
+		}
 
-        internal void HandleRandomFalloff()
-        {
-            if(wear <= 0)
-            {
-                part.removePart();
-            }
+		internal void HandleRandomFalloff()
+		{
+			if (wear <= 0)
+			{
+				part.removePart();
+			}
 
-            if(randomFallOff != -1)
-            {
-                int randomValue = new Random().Next(randomFallOff);
-                if(randomValue == 1)
-                {
-                    part.removePart();
-                }
-            }
-        }
-        internal static void Save(Mod mod, string saveFile, Wear[] wears)
-        {
-            try
-            {
-                Dictionary<string, float> save = new Dictionary<string, float>();
-                foreach (Wear wear in wears)
-                {
-                    save[wear.id] = wear.wear;
-                }
-                SaveLoad.SerializeSaveFile<Dictionary<string, float>>(mod, save, saveFile);
-            }
-            catch(Exception ex)
-            {
-                Logger.New("Error while trying to save wear information", ex);
-            }
-        }
-    }
+			if (randomFallOff != -1)
+			{
+				int randomValue = new Random().Next(randomFallOff);
+				if (randomValue == 1)
+				{
+					part.removePart();
+				}
+			}
+		}
+		internal static void Save(Mod mod, string saveFile, Wear[] wears)
+		{
+			try
+			{
+				Dictionary<string, float> save = new Dictionary<string, float>();
+				foreach (Wear wear in wears)
+				{
+					save[wear.id] = wear.wear;
+				}
+				SaveLoad.SerializeSaveFile<Dictionary<string, float>>(mod, save, saveFile);
+			}
+			catch (Exception ex)
+			{
+				Logger.New("Error while trying to save wear information", ex);
+			}
+		}
+	}
+	*/
 }

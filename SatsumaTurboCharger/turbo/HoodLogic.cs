baa1a15648dd0fe123965d887e4cd70009bb37d3
Marvin Beym <Marvin.Beym@gmx.de>
2023-08-16 18:23:01 +0200

    Fix turbo big hood not assembling/disassembling properly because of HingeJoint

--- SatsumaTurboCharger/turbo/HoodLogic.cs
+++ SatsumaTurboCharger/turbo/HoodLogic.cs
@@ -2,7 +2,9 @@
 using MscModApi.Caching;
 using MscModApi.Parts;
 using MscModApi.Tools;
+using SatsumaTurboCharger.part;
 using UnityEngine;
+using static SatsumaTurboCharger.part.TurboBigHood;
 using EventType = MscModApi.Parts.EventType;
 
 
@@ -10,16 +12,7 @@ namespace SatsumaTurboCharger
 {
 	public class HoodLogic : MonoBehaviour
 	{
-		protected enum OpenState
-		{
-			Closed = 0,
-			Open = 87,
-			Lifted = 2
-		}
-
-		private Part turboHood;
-		private GameObject latchCollider;
-		private HingeJoint hingeJoint;
+		private TurboBigHood turboHood;
 
 		protected OpenState openState = OpenState.Closed;
 
@@ -39,51 +32,16 @@ namespace SatsumaTurboCharger
 
 				SetHoodState(openState == OpenState.Closed ? OpenState.Lifted : OpenState.Closed);
 			});
-
-			latchCollider = GameObject.CreatePrimitive(PrimitiveType.Cube);
-			latchCollider.name = turboHood.name + "_LATCHCOLLIDER";
-			latchCollider.transform.SetParent(turboHood.transform);
-			latchCollider.GetComponent<Collider>().isTrigger = true;
-			latchCollider.GetComponent<Renderer>().enabled = false;
-			latchCollider.transform.localPosition = new Vector3(0f, 0.06f, 0.36f);
-			latchCollider.transform.localEulerAngles = new Vector3(352f, 0f, 0f);
-			latchCollider.transform.localScale = new Vector3(1.21f, 0.05f, 0.85f);
-
-			turboHood.AddEventListener(EventTime.Post, EventType.Install, () =>
-			{
-				latchCollider.SetActive(true);
-				hingeJoint = turboHood.AddComponent<HingeJoint>();
-
-				hingeJoint.connectedBody = CarH.satsuma.GetComponent<Rigidbody>();
-				hingeJoint.anchor = new Vector3(0, 0, 0);
-				hingeJoint.axis = new Vector3(0, 0, 0);
-				hingeJoint.useSpring = true;
-				hingeJoint.useLimits = true;
-				hingeJoint.enableCollision = false; 
-				hingeJoint.limits = new JointLimits
-				{
-					min = -(float) OpenState.Open,
-					max = (float) OpenState.Closed,
-				};
-
-				SetHoodState(OpenState.Closed);
-			});
-
-			turboHood.AddEventListener(EventTime.Pre, EventType.Uninstall, () =>
-			{
-				latchCollider.SetActive(false);
-				Destroy(hingeJoint);
-			});
 		}
 
-		public void Init(Part turboHood)
+		public void Init(TurboBigHood turboHood)
 		{
 			this.turboHood = turboHood;
 		}
 
 		void LateUpdate()
 		{
-			if (openState == OpenState.Closed || !latchCollider.IsLookingAt())
+			if (openState == OpenState.Closed || !turboHood.latchCollider.IsLookingAt())
 			{
 				return;
 			}
@@ -95,13 +53,13 @@ namespace SatsumaTurboCharger
 			}
 		}
 
-		protected void SetHoodState(OpenState state)
+		public void SetHoodState(OpenState state)
 		{
-			JointSpring spring = hingeJoint.spring;
+			JointSpring spring = turboHood.hingeJoint.spring;
 			spring.spring = springSpeed;
 			spring.damper = springDamping;
 			spring.targetPosition = (float) state;
-			hingeJoint.spring = spring;
+			turboHood.hingeJoint.spring = spring;
 
 			openState = state;
 		}
